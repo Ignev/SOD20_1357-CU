64cb440ccf836de563d539d067b38a561a110445
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-03 20:05:25 +0300

    new text banner

--- script.js
+++ script.js
@@ -6,52 +6,52 @@ let banners = [
     bg: "./assets/img/bg-0.jpg",
     bannerText:
       "<strong>Oh, eine Sprachpanne.</strong> <br/> <span>Sprechen Sie noch einmal</span>",
-    subtext: "Abbildung zeigt Sonderausstattung gegen Mehrpreis",
+    subtext: '<img style="width: 210px; height: 15px;" src="./assets/fonts/Abbildung zeigt Sonderausstattung gegen Mehrpreis..svg" alt="" class="banner__subtext">',
   },
   {
     step: 1,
     bg: "./assets/img/bg-1.jpg",
     bannerText:
-      "<strong>Der neue <br /> Caddy Cargo</strong> <br/> <span>ereit für alles, <br />was kommt</span>",
-    subtext: "Abbildung zeigt Sonderausstattung gegen Mehrpreis",
+      '<img style="width: 155px; height: 63px;" src="./assets/fonts/Der neue Caddy Cargo.svg" alt="" class="text__strong"><img src="./assets/fonts/Bereit für alles, was kommt.svg" style="width: 160px; height: 50px;" alt="" class="text__span">',
+    subtext: '<img style="width: 210px; height: 10px;" src="./assets/fonts/Abbildung zeigt Sonderausstattung gegen Mehrpreis..svg" alt="" class="banner__subtext">',
   },
   {
     step: 2,
     bg: "./assets/img/bg-2.jpg",
     bannerText:
-      "<strong>Der neue <br /> Caddy Cargo</strong> <br/> <span>LED Scheinwerfer*</span>",
+      '<img src="./assets/fonts/Der neue Caddy Cargo.svg" alt="" class="text__strong"><img style="width: 150px; height: 20px; " src="./assets/fonts/LED Scheinwerfer_.svg" alt="" class="text__span">',
     popupImg: "./assets/video/video-4.mp4",
     popupText:
       "Alles im Blick: Mit den neuen LED Frontscheinwerfern* <br/> und LED Rückleuchten* entgeht Ihnen nichts mehr.",
     top: "40%",
     right: "40px",
     subtext:
-      "Abbildung zeigt Sonderausstattung gegen Mehrpreis.<br/> *Sonderausstattung gegen Mehrpreis.",
+    '<img style="width: 210px; height: 10px;" src="./assets/fonts/Abbildung zeigt Sonderausstattung gegen Mehrpreis..svg" alt="" class="banner__first-media"><img style="width: 150px; height: 10px;" src="./assets/fonts/_Sonderausstattung gegen Mehrpreis..svg" alt="" class="second-media">',
   },
   {
     step: 3,
     bg: "./assets/img/bg-3.jpg",
     bannerText:
-      "<strong>Der neue Caddy Cargo</strong> <br/> <span>Großer Laderaum</span>",
+    '<img src="./assets/fonts/Der neue Caddy Cargo.svg" alt="" class="text__strong"><img style="width: 160px; height: 20px;" src="./assets/fonts/Großer Laderaum.svg" alt="" class="text__span">',
     popupImg: "./assets/video/video-1.mp4",
     popupText:
       "Innen überzeugt die fünfte Generation des Caddy Cargo <br/> mit neuen Hightech-Lösungen und deutlich mehr Platz.",
     top: "40%",
     right: "100px",
-    subtext: "Abbildungen zeigen Sonderausstattung gegen Mehrpreis.",
+    subtext: '<img style="width: 210px; height: 10px;" src="./assets/fonts/Abbildung zeigt Sonderausstattung gegen Mehrpreis..svg" alt="" class="banner__first-media">',
   },
   {
     step: 4,
     bg: "./assets/img/bg-4.jpg",
     bannerText:
-      "<strong>Der neue <br/>Caddy Cargo Maxi</strong> <br/> <span>Breitere <br/>Schiebetür*</span>",
+    '<img src="./assets/fonts/Der neue Caddy Cargo.svg" alt="" class="text__strong"><img style="width: 125px; height: 50px;" src="./assets/fonts/Breitere Schiebetür_.svg" alt="" class="text__span">',
     popupImg: "./assets/video/video-2.mp4",
     popupText:
       "Dank extrabreiter Schiebetür* können jetzt <br/> Europaletten sogar seitlich eingeladen werden.",
     top: "30%",
     right: "50px",
     subtext:
-      "Abbildung zeigt Sonderausstattung gegen Mehrpreis.<br/> *Sonderausstattung gegen Mehrpreis.",
+    '<img style="width: 210px; height: 10px;" src="./assets/fonts/Abbildung zeigt Sonderausstattung gegen Mehrpreis..svg" alt="" class="banner__first-media"><img style="width: 150px; height: 10px;" src="./assets/fonts/_Sonderausstattung gegen Mehrpreis..svg" alt="" class="second-media">',
   },
 ];
 if (isChrome) {
