62778155b11eb228d5512f61e893041f3d21dcf6
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-04 14:14:48 +0300

    add 5 slide

--- script.js
+++ script.js
@@ -52,6 +52,18 @@ let banners = [
     subtext:
     '<img style="width: 210px; height: 10px;" src="./assets/fonts/Abbildung-zeigt-Sonderausstattung-gegen-Mehrpreis.svg" alt="" class="banner__first-media"><img style="width: 150px; height: 10px;" src="./assets/fonts/Sonderausstattung-gegen-Mehrpreis.svg" alt="" class="second-media">',
   },
+  {
+    step: 5,
+    bg: "./assets/img/bg-5.jpg",
+    bannerText:
+    '<img src="./assets/fonts/Der-neue-Caddy-Cargo.svg" alt="" class="text__strong"><img style="width: 125px; height: 20px;" src="./assets/fonts/Lane-Assist.svg" alt="" class="text__span">',
+    popupImg: "./assets/video/video-3.mp4",
+    popupText:
+      "Der Lane Assist* erfasst und korrigiert autonom bei drohender Überfahrung der Spurbegrenzungslinien.",
+    top: "30%",
+    right: "50px",
+    subtext:'<img style="width: 210px; height: 10px;" src="./assets/fonts/Abbildung-zeigt-Sonderausstattung-gegen-Mehrpreis.svg" alt="" class="banner__first-media"><img style="width: 150px; height: 10px;" src="./assets/fonts/Sonderausstattung-gegen-Mehrpreis.svg" alt="" class="second-media">'
+  },
 ];
   var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
   var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
@@ -91,10 +103,10 @@ const showBanner = (
       setTimeout(() => {
         bubble.classList.remove('bunner__bubble-active');
       }, 2130);
-    if (banner.dataset.step <= 4) {
+    if (banner.dataset.step <= 5) {
       banner.dataset.step++;
     }
-    if (banner.dataset.step == 5) {
+    if (banner.dataset.step == 6) {
       banner.dataset.step = 2;
     }
     if (banner.dataset.step >= 2) {
